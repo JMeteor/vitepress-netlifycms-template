358e57f69a4feb7280aa2c929fd83626fc4d7ed8
jmeteor <jerzy.zientek@gmail.com>
2022-02-10 21:34:35 +0100

    Add custom layout and postcss nesting support

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'VitePress + NetlifyCMS',
+  description: 'Just playing around but seriously this is the best template ever',
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  },
+}
---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    Site Header
+    <a href="#">Test Link</a>
+  </header>
+  <main>
+    <Content />
+  </main>
+  <footer>
+    Site Footer
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Layout"
+}
+</script>
+
+<style src="./main.css" />
