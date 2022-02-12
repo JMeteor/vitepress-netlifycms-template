7f234522270342dcc9eec45daded400b4de90812
jmeteor <jerzy.zientek@gmail.com>
2022-02-12 11:21:50 +0100

    Add Netlify Identity Widget

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,15 @@ module.exports = {
   title: "VitePress + NetlifyCMS",
   description:
     "Just playing around but seriously this is the best template ever",
+  head: [
+    ['script', { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js'}]
+  ],
   themeConfig: {
+    repo: 'JMeteor/vitepress-netlifycms-template',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
     // Type is `DefaultTheme.Config`
   },
 };
